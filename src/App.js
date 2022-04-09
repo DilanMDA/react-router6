35b11db4f0e60718fc4c5949ebe33adec641cfaf
DilanMDA <akiladilan04@gmail.com>
2022-04-09 12:16:53 +0530

    setup page content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<div>Home page</div>} />
-        <Route exact path='/about' element={<div>about</div>} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/products' element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
-const Home = () => {
+import React from 'react';
+
+const Home = () =>
+{
   return (
     <section className='section'>
       <h2>Home Page</h2>
