9999e1e329056517d1aa06633737ce2a8b7763c7
DilanMDA <akiladilan04@gmail.com>
2022-04-09 12:50:06 +0530

    Navigate the about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+// import About from './About';
 
-const Home = () =>
-{
+const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Link to='/about' className='btn'>
+        About
+      </Link>
     </section>
   );
 };
